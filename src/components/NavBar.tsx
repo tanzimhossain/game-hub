375d4dcf71b58e77f217c38aa270dded9201a8dd
Md. Tanzim Hossain <tanzim.7400@gmail.com>
2025-09-24 15:50:05 +0200

    Update dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} alt="logo" height="50px" objectFit="contain" />
-      <Text fontSize="xl" fontWeight="bold">
-        GameHub
-      </Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
